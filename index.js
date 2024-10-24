656310cf062d9a3cc6d75501dfaf851c53d5fbd1
Jeriyl Florence <jeriyl94@gmail.com>
2024-10-24 19:37:40 +0530

    Input from user

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('Hello World!');
+
+let fname = "Jerry";
+let age = 29;
+let maritalStatus = true;
+
+console.log(fname, age, maritalStatus);
+
+document.getElementById('p1').innerHTML = 'Hello ' + fname;
+document.getElementById('p2').innerHTML = fname + " is " + age + " years old.";
+document.getElementById('p3').innerHTML = fname + ' maritalStatus is ' + maritalStatus;
+
+// window prompt : Input from user
+/*
+let username = window.prompt('What is your name?');
+console.log('Window prompt message ' + username);
+*/
+
+document.getElementById('btn').onclick = function () {
+    username = document.getElementById('name').value;
+    console.log('Button clicked ' + username);
+    document.getElementById('p4').innerHTML = 'Hello ' + username;
+}
